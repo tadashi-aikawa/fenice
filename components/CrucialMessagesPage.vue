7126190a94fc8525251562f21bdde6943563b181
tadashi-aikawa <syou.maman@gmail.com>
2024-04-03 23:00:36 +0900

    カードの表示・既読消去時にアニメーションさせる

--- components/CrucialMessagesPage.vue
+++ components/CrucialMessagesPage.vue
@@ -15,7 +15,25 @@ onMounted(async () => {
 <template>
   <div class="d-flex flex-column align-center pa-5">
     <div style="width: 750px; height: 95vh; overflow-y: auto">
-      <PostCard v-for="message in messages" :message="message" />
+      <transition-group name="list">
+        <PostCard
+          :key="message.ts"
+          v-for="message in messages"
+          :message="message"
+        />
+      </transition-group>
     </div>
   </div>
 </template>
+
+<style scoped>
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.3s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translatex(360px);
+}
+</style>
