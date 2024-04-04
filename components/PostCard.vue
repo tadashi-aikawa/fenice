ab8618a5f3b7d42fecf3c8b5bac59f816ccaf4a2
tadashi-aikawa <syou.maman@gmail.com>
2024-04-04 23:06:49 +0900

    ポストカードのデザインを未読ボタンが左になるように刷新

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -44,37 +44,52 @@ ${x.fallback}
 </script>
 
 <template>
-  <v-card variant="flat" max-width="720" class="mb-5" :elevation="4">
-    <v-card-item>
-      <div class="d-flex align-center my-1 ga-2">
-        <div class="text-body-2 font-weight-bold">
-          {{ message.username }}
+  <v-card variant="elevated" max-width="720" class="mb-3" :elevation="2">
+    <v-card-item class="ma-0 pa-0">
+      <div class="d-flex">
+        <div class="read-button" @click="handleRead">
+          <v-icon size="large">mdi-check-circle-outline</v-icon>
         </div>
-        <v-spacer />
-        <v-btn
-          icon="mdi-check-circle-outline"
-          @click="handleRead"
-          variant="tonal"
-          density="compact"
-          style="color: forestgreen"
-        />
-        <v-btn
-          icon="mdi-slack"
-          @click="handleOpenSlack"
-          variant="tonal"
-          density="compact"
-          style="color: goldenrod"
-        />
-      </div>
 
-      <v-divider class="pb-3" />
-      <pre class="text-caption" style="white-space: pre-wrap">{{
-        displayMessage
-      }}</pre>
-      <div class="d-flex justify-end text-grey-darken-1 ga-1 mt-3">
-        <v-icon>mdi-clock</v-icon>
-        <span>{{ ts2display(message.ts) }}</span>
+        <div style="width: 100%" class="px-3 pt-1 pb-2">
+          <div class="d-flex align-center my-1 ga-2">
+            <div class="text-body-2 font-weight-bold">
+              {{ message.username }}
+            </div>
+            <v-spacer />
+            <v-btn
+              icon="mdi-slack"
+              @click="handleOpenSlack"
+              variant="tonal"
+              density="compact"
+              style="color: goldenrod"
+            />
+          </div>
+
+          <v-divider class="pb-3" />
+          <pre class="text-caption" style="white-space: pre-wrap">{{
+            displayMessage
+          }}</pre>
+          <div class="d-flex justify-end text-grey-darken-1 ga-1 mt-3">
+            <v-icon>mdi-clock</v-icon>
+            <span>{{ ts2display(message.ts) }}</span>
+          </div>
+        </div>
       </div>
     </v-card-item>
   </v-card>
 </template>
+
+<style scoped>
+.read-button {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 0 10px;
+  color: darkgrey;
+  border-right: dashed 1px lightgrey;
+}
+.read-button:hover {
+  background-color: rgba(144, 188, 144, 0.4);
+}
+</style>
