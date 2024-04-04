b630502ab1f8c7636f4c7ee72dffbe295e7b203c
tadashi-aikawa <syou.maman@gmail.com>
2024-04-04 22:50:12 +0900

    警告の除去

--- components/Badge.vue
+++ components/Badge.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+});
+
 defineProps<{
   disabled: boolean;
 }>();
 </script>
 
 <template>
-  <v-badge v-if="!disabled">
+  <v-badge v-if="!disabled" v-bind="$attrs">
     <slot></slot>
   </v-badge>
   <template v-else>
