a4f2e349d488f43d8b656d951341acd6ed6336fe
tadashi-aikawa <syou.maman@gmail.com>
2024-04-11 22:38:28 +0900

    通知をクリックしてもFeniceのウィンドウ/タブ/重要メッセージ画面がアクティブにならないことがある不具合修正

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -40,6 +40,29 @@ async function searchMessages(
 }
 
 export default defineBackground(() => {
+  const notifiedIds = new Set<string>();
+  browser.notifications.onClicked.addListener(async (nid) => {
+    if (!notifiedIds.has(nid)) {
+      return;
+    }
+    notifiedIds.delete(nid);
+
+    const feniceTab = (await browser.tabs.query({ title: "Fenice" })).at(0);
+    if (!feniceTab?.id) {
+      return;
+    }
+
+    const windowId = (await browser.windows.get(feniceTab.windowId!))?.id;
+    if (windowId) {
+      browser.windows.update(windowId, { focused: true });
+    }
+
+    browser.tabs.update(feniceTab.id, {
+      active: true,
+    });
+    browser.tabs.sendMessage(feniceTab.id, { page: "crucial-messages" });
+  });
+
   // FIXME: オプションで設定できるようにしたい
   browser.alarms.create("", { periodInMinutes: 1 });
   browser.alarms.onAlarm.addListener(async (_alarm) => {
@@ -99,15 +122,7 @@ export default defineBackground(() => {
       type: "basic",
       iconUrl: FENICE_ICON_URL,
     });
-    browser.notifications.onClicked.addListener((nid) => {
-      if (notificationId !== nid) {
-        return;
-      }
-      browser.tabs.update(feniceTab.id, {
-        active: true,
-      });
-      browser.tabs.sendMessage(feniceTab.id!, { page: "crucial-messages" });
-    });
+    notifiedIds.add(notificationId);
 
     const newUnreadMessages = unreadMessages
       .concat(newMessages)
