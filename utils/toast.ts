c47334aa0eca411650edd85d69648f027d12c7dd
tadashi-aikawa <syou.maman@gmail.com>
2024-05-09 23:24:48 +0900

    成功トーストのアニメーションを変更

--- utils/toast.ts
+++ utils/toast.ts
@@ -20,19 +20,9 @@ export function showErrorToast(arg: RequestError | string): Id {
 export function showSuccessToast(message: string): Id {
   return toast.success(message, {
     autoClose: 3000,
-    transition: "flip",
+    transition: "slide",
     position: "top-center",
     pauseOnFocusLoss: false,
     hideProgressBar: true,
-  });
-}
-
-export function showInfoToast(message: string): Id {
-  return toast.info(message, {
-    autoClose: 3000,
-    transition: "bounce",
-    pauseOnFocusLoss: false,
-    hideProgressBar: true,
-    theme: "colored",
   });
 }
