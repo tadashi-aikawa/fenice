84ad5e3759bb250898eb1a3925624c45c33196cd
tadashi-aikawa <syou.maman@gmail.com>
2024-08-12 16:00:31 +0900

    Grid Layoutの使用

--- components/ZenTimesPage.vue
+++ components/ZenTimesPage.vue
@@ -12,15 +12,34 @@ const lockOnMessage = computed(() =>
 </script>
 
 <template>
-  <div class="d-flex justify-center ga-6 ma-6">
-    <DestToggle v-model="dest" />
+  <div class="grid-container">
+    <DestToggle v-model="dest" class="dest" />
 
-    <div v-if="dest">
-      <PostForm :dest="dest" />
-      <div v-if="lockOnMessage" class="d-flex flex-column align-center">
-        <span class="py-3" style="font-size: 24px">↓</span>
-        <PostCard :message="lockOnMessage" />
-      </div>
+    <div class="form">
+      <template v-if="dest">
+        <PostForm :dest="dest" />
+        <div v-if="lockOnMessage" class="d-flex flex-column align-center">
+          <span class="py-3" style="font-size: 24px">↓</span>
+          <PostCard :message="lockOnMessage" />
+        </div>
+      </template>
     </div>
   </div>
 </template>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template:
+    " .... ....  .... ....  ...." 45px
+    " .... dest  .... form  ...." auto
+    " .... ....  .... ....  ...." 45px
+    / auto 300px 30px 680px auto;
+}
+.dest {
+  grid-area: dest;
+}
+.form {
+  grid-area: form;
+}
+</style>
