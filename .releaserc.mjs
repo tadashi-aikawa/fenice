46adf7acd2eb3aa0cbc984a1e6290f2f7fb3f451
tadashi-aikawa <syou.maman@gmail.com>
2024-10-30 12:38:39 +0900

    ci: GitHubリリースページに成果物が添付されない不備を修正

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -46,7 +46,7 @@ export default {
     [
       "@semantic-release/github",
       {
-        assets: [".output/fenice-${nextRelease.version}-chrome.zip"],
+        assets: [".output/fenice-*-chrome.zip"],
       },
     ],
     "@semantic-release/git",
